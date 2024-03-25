363e9939c3bb2746f3815f3f0d52ba1dd2ba571b
kanal-boglarka <kanalboglarka@gmail.com>
2024-03-25 23:18:34 +0100

    fix: implement toString() in Nationality with ReflectionToStringBuilder

--- src/main/java/nationalize/Nationality.java
+++ src/main/java/nationalize/Nationality.java
@@ -1,4 +1,6 @@
 package nationalize;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
 import com.google.gson.annotations.SerializedName;
 
@@ -68,6 +70,16 @@ public class Nationality {
             this.probability = probability;
         }
 
+        @Override
+        public String toString() {
+            return ReflectionToStringBuilder.toString(this, ToStringStyle.NO_CLASS_NAME_STYLE);
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return ReflectionToStringBuilder.toString(this, ToStringStyle.NO_CLASS_NAME_STYLE);
     }
 
 }
